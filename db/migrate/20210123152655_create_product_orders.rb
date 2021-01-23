b3ba6a7eef8b889f7ce57d911c57fec05e83f799
jdhofmann <justindhofmann@gmail.com>
2021-01-23 10:29:21 -0500

    creating tables

---
+++ db/migrate/20210123152655_create_product_orders.rb
@@ -0,0 +1,11 @@
+class CreateProductOrders < ActiveRecord::Migration[6.0]
+  def change
+    create_table :product_orders do |t|
+      t.integer :quantity
+      t.belongs_to :sku, null: false, foreign_key: true
+      t.belongs_to :order, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
