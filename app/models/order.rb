22494f0873545d1f8ac8110debd508f519b481cc
jdhofmann <justindhofmann@gmail.com>
2021-01-25 17:26:19 -0500

    price total

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :product_orders
+
+  def price_total 
+    100
+  end 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_many :orders 
     has_many :product_orders, through: :orders
+
 end
 
